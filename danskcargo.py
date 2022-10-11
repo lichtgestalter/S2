cb3277076942d35419c660c0a849ca07f53806bf
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-10-11 11:46:22 +0200

    select_all(Transport)

--- danskcargo.py
+++ danskcargo.py
@@ -13,6 +13,8 @@ from sqlalchemy import select
 from sqlalchemy import update
 from sqlalchemy import delete
 
+from datetime import date
+
 Database = 'sqlite:///dc1.db'
 Base = declarative_base()  # creating the registry and declarative base classes - combined into one step. Base will serve as the base class for the ORM mapped classes we declare.
 
@@ -24,7 +26,7 @@ class Container(Base):
     destination = Column(String)
 
     def __repr__(self):
-        return f"Container({self.id=:4}    {self.weight=:6}    {self.destination=})"
+        return f"Container({self.id=:4}    {self.weight=:16}    {self.destination=})"
 
 
 class Aircraft(Base):
@@ -34,7 +36,7 @@ class Aircraft(Base):
     registration = Column(String)
 
     def __repr__(self):
-        return f"Aircraft({self.id=:4}, {self.max_cargo_weight=:6}, {self.registration=})"
+        return f"Aircraft ({self.id=:4},   {self.max_cargo_weight=:6},  {self.registration=})"
 
 
 class Transport(Base):
@@ -45,18 +47,20 @@ class Transport(Base):
     aircraft_id = Column(Integer, ForeignKey("aircraft.id"), nullable=False)
 
     def __repr__(self):
-        return f"Transporter({self.id=}, {self.date=}, {self.container=}, {self.aircraft=})"
+        return f"Transporter({self.id=}, {self.date=}, {self.container_id=}, {self.aircraft_id=})"
 
 
 def create_test_data_1():
     with Session(engine) as session:
         new_items = []
-        new_items.append(Container(weight=1200, destination="Oslo"))
-        new_items.append(Container(weight=700, destination="Helsinki"))
-        new_items.append(Container(weight=1800, destination="Helsinki"))
-        new_items.append(Container(weight=1000, destination="Helsinki"))
-        new_items.append(Aircraft(max_cargo_weight=2000, registration="OY-CBS"))
-        new_items.append(Aircraft(max_cargo_weight=3000, registration="OY-THR"))
+        # new_items.append(Container(weight=1200, destination="Oslo"))
+        # new_items.append(Container(weight=700, destination="Helsinki"))
+        # new_items.append(Container(weight=1800, destination="Helsinki"))
+        # new_items.append(Container(weight=1000, destination="Helsinki"))
+        # new_items.append(Aircraft(max_cargo_weight=2000, registration="OY-CBS"))
+        # new_items.append(Aircraft(max_cargo_weight=3000, registration="OY-THR"))
+        a_date = date(day=10, month=12, year=2022)
+        new_items.append(Transport(date=a_date, container_id=2, aircraft_id=3))
         session.add_all(new_items)
         session.commit()
 
@@ -82,9 +86,9 @@ def select_all(classparam):  # https://docs.sqlalchemy.org/en/14/tutorial/data_s
     return result
 
 
-def get_record(classparam, id):  # https://docs.sqlalchemy.org/en/14/tutorial/data_select.html
+def get_record(classparam, record_id):  # https://docs.sqlalchemy.org/en/14/tutorial/data_select.html
     with Session(engine) as session:
-        record = session.scalars(select(classparam).where(classparam.id == id)).first()  # very useful for converting into our data class
+        record = session.scalars(select(classparam).where(classparam.id == record_id)).first()  # very useful for converting into our data class
     return record
 
 
@@ -127,11 +131,12 @@ def insert_example():  # https://docs.sqlalchemy.org/en/14/tutorial/orm_data_man
 
 engine = create_engine(Database, echo=False, future=True)  # https://docs.sqlalchemy.org/en/14/tutorial/engine.html   The start of any SQLAlchemy application is an object called the Engine. This object acts as a central source of connections to a particular database, providing both a factory as well as a holding space called a connection pool for these database connections. The engine is typically a global object created just once for a particular database server, and is configured using a URL string which will describe how it should connect to the database host or backend.
 Base.metadata.create_all(engine)
-# create_test_data_1(engine)
+# create_test_data_1()
 # select_all(Container)
 # select_all(Aircraft)
+select_all(Transport)
 print(get_record(Container, 2))
-print(get_record(Aircraft, 2))
+print(get_record(Aircraft, 3))
 # update_example(engine)
 # delete_example(engine)
 # insert_example(engine)
