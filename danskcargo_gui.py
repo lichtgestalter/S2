cb69a2cd42e1c75852a781a005512ce40e03bce9
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-10-13 11:37:39 +0200

    def edit_container():

--- danskcargo_gui.py
+++ danskcargo_gui.py
@@ -4,57 +4,56 @@ from tkinter import messagebox
 import danskcargo_sql as dcsql
 
 
-def remove_selected():  # Remove Many records
-    x = my_tree.selection()
+def remove_selected_container():  # Remove Many records
+    x = tree_container.selection()
     for record in x:
-        my_tree.delete(record)  # delete from treeview
+        tree_container.delete(record)  # delete from treeview
         # kode mangler  # delete from database
 
 
-def clear_entries():  # Clear entry boxes
+def clear_container_entries():  # Clear entry boxes
     entry_id.delete(0, END)
     entry_weight.delete(0, END)
     entry_destination.delete(0, END)
 
 
-def select_record(e):  # Select Record
-    entry_id.delete(0, END)  # Clear entry boxes
-    entry_weight.delete(0, END)
-    entry_destination.delete(0, END)
-
-    selected = my_tree.focus()  # Grab record Number
-    values = my_tree.item(selected, 'values')  # Grab record values
-
+def fill_container_entries(values):  # Fill entry boxes
     entry_id.insert(0, values[0])  # output to entry boxes
     entry_weight.insert(0, values[1])
     entry_destination.insert(0, values[2])
 
 
-def update_record():
-    selected = my_tree.focus()  # Grab the record number
+def edit_container():  # Select record
+    clear_container_entries()  # Clear entry boxes
+    selected = tree_container.focus()  # Grab record number
+    values = tree_container.item(selected, 'values')  # Grab record values
+    fill_container_entries(values)
+
+
+def update_container():
+    selected = tree_container.focus()  # Grab the record number
     record = (entry_id.get(), entry_weight.get(), entry_destination.get(),)
-    my_tree.item(selected, text="", values=record)  # update treeview
+    tree_container.item(selected, text="", values=record)  # update treeview
     # kode mangler  # Update database
-    # clear_entries()  # Clear entry boxes
-5
+    clear_container_entries()  # Clear entry boxes
+    refresh_container()
 
-# add new record to database
-def add_record():
+
+def add_container():  # add new record to database
     record = (entry_id.get(), entry_weight.get(), entry_destination.get(),)
     # kode mangler  # Update database
-    clear_entries()  # Clear entry boxes
-    my_tree.delete(*my_tree.get_children())  # Clear The Treeview Table
-    # kode mangler  # fill tree from database
+    clear_container_entries()  # Clear entry boxes
+    refresh_container()
 
 
-def data2table(tree):
+def read_container():  # fill tree from database
     count = 0
     result = dcsql.select_all(dcsql.Container)
     for record in result:
         if count % 2 == 0:
-            tree.insert(parent='', index='end', iid=count, text='', values=(record.id, record.weight, record.destination), tags=('evenrow',))
+            tree_container.insert(parent='', index='end', iid=count, text='', values=(record.id, record.weight, record.destination), tags=('evenrow',))
         else:
-            tree.insert(parent='', index='end', iid=count, text='', values=(record.id, record.weight, record.destination), tags=('oddrow',))
+            tree_container.insert(parent='', index='end', iid=count, text='', values=(record.id, record.weight, record.destination), tags=('oddrow',))
         count += 1
 
 
@@ -62,40 +61,42 @@ def empty_table(tree):
     tree.delete(*tree.get_children())
 
 
+def refresh_container():
+    empty_table(tree_container)  # Clear The Treeview Table
+    read_container()  # fill tree from database
+
+
 root = Tk()
 root.title('AspIT S2: DanskCargo')
 root.iconbitmap('AspIT.ico')
 root.geometry("1000x500")
-
-data = [[1, 1000, "Las Vegas"], [2, 2222, "Chicago"]]  # Add fake data
+# data = [[1, 1000, "Las Vegas"], [2, 2222, "Chicago"]]  # Add fake data
 
 style = ttk.Style()  # Add style
 style.theme_use('default')  # Pick theme
 style.configure("Treeview", background="#D3D3D3", foreground="black", rowheight=25, fieldbackground="#D3D3D3")  # Configure Treeview Colors
-style.map('Treeview', background=[('selected', "#347083")])  # Change selected color
+style.map('Treeview', background=[('selected', "#309070")])  # Change selected color
 
-tree_frame = Frame(root)  # Create treeview frame
-tree_frame.pack(pady=10)
-tree_scroll = Scrollbar(tree_frame)  # Create treeview scrollbar
-tree_scroll.pack(side=RIGHT, fill=Y)
-my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")  # Create treeview
-my_tree.pack()
-tree_scroll.config(command=my_tree.yview)  # Configure scrollbar
+tree_frame_container = Frame(root)  # Create treeview frame
+tree_frame_container.pack(pady=10)
+tree_scroll_container = Scrollbar(tree_frame_container)  # Create treeview scrollbar
+tree_scroll_container.pack(side=RIGHT, fill=Y)
+tree_container = ttk.Treeview(tree_frame_container, yscrollcommand=tree_scroll_container.set, selectmode="extended")  # Create treeview
+tree_container.pack()
+tree_scroll_container.config(command=tree_container.yview)  # Configure scrollbar
 
-my_tree['columns'] = ("id", "weight", "destination")  # Define columns
+tree_container['columns'] = ("id", "weight", "destination")  # Define columns
+tree_container.column("#0", width=0, stretch=NO)  # Format columns
+tree_container.column("id", anchor=E, width=40)
+tree_container.column("weight", anchor=E, width=80)
+tree_container.column("destination", anchor=W, width=200)
+tree_container.heading("#0", text="", anchor=W)  # Create headings
+tree_container.heading("id", text="Id", anchor=CENTER)
+tree_container.heading("weight", text="Weight", anchor=CENTER)
+tree_container.heading("destination", text="Destination", anchor=CENTER)
 
-my_tree.column("#0", width=0, stretch=NO)  # Format columns
-my_tree.column("id", anchor=E, width=40)
-my_tree.column("weight", anchor=E, width=80)
-my_tree.column("destination", anchor=W, width=200)
-
-my_tree.heading("#0", text="", anchor=W)  # Create headings
-my_tree.heading("id", text="Id", anchor=CENTER)
-my_tree.heading("weight", text="Weight", anchor=CENTER)
-my_tree.heading("destination", text="Destination", anchor=CENTER)
-
-my_tree.tag_configure('oddrow', background="#dddddd")  # Create striped row tags
-my_tree.tag_configure('evenrow', background="#cccccc")
+tree_container.tag_configure('oddrow', background="#dddddd")  # Create striped row tags
+tree_container.tag_configure('evenrow', background="#cccccc")
 
 data_frame = LabelFrame(root, text="Record")  # Add record entry boxes
 data_frame.pack(fill="x", expand="yes", padx=20)
@@ -118,14 +119,17 @@ entry_destination.grid(row=0, column=5, padx=10, pady=10)
 button_frame = LabelFrame(root, text="Commands")  # Add Buttons
 button_frame.pack(fill="x", expand="yes", padx=20)
 
-update_button = Button(button_frame, text="Update Record", command=update_record)
-update_button.grid(row=0, column=0, padx=10, pady=10)
+button_edit_container = Button(button_frame, text="Edit Container", command=edit_container)
+button_edit_container.grid(row=0, column=1, padx=10, pady=10)
 
-add_button = Button(button_frame, text="Add Record", command=add_record)
-add_button.grid(row=0, column=1, padx=10, pady=10)
+button_update_container = Button(button_frame, text="Update Record", command=update_container)
+button_update_container.grid(row=0, column=3, padx=10, pady=10)
 
-remove_all_button = Button(button_frame, text="Remove Selected", command=remove_selected)
-remove_all_button.grid(row=0, column=2, padx=10, pady=10)
+add_button = Button(button_frame, text="Add Record", command=add_container)
+add_button.grid(row=0, column=5, padx=10, pady=10)
+
+remove_all_button = Button(button_frame, text="Remove Selected", command=remove_selected_container)
+remove_all_button.grid(row=0, column=7, padx=10, pady=10)
 
 # remove_one_button = Button(button_frame, text="Remove One Selected", command=remove_one)
 # remove_one_button.grid(row=0, column=3, padx=10, pady=10)
@@ -139,13 +143,10 @@ remove_all_button.grid(row=0, column=2, padx=10, pady=10)
 # move_down_button = Button(button_frame, text="Move Down", command=down)
 # move_down_button.grid(row=0, column=6, padx=10, pady=10)
 
-select_record_button = Button(button_frame, text="Clear Entry Boxes", command=clear_entries)
+select_record_button = Button(button_frame, text="Clear Entry Boxes", command=clear_container_entries)
 select_record_button.grid(row=0, column=7, padx=10, pady=10)
 
-# Bind the treeview
-# my_tree.bind("", select_record)
+tree_container.bind("select", edit_container)  # Bind the treeview
 
-
-empty_table(my_tree)
-data2table(my_tree)
+refresh_container()
 root.mainloop()
